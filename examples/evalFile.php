db32779cb39bf6399002abbf8e844575179dc5a3
Gu Weigang <guweigang@outlook.com>
2017-05-12 22:29:01 +0800

    modify examples

--- examples/evalFile.php
+++ examples/evalFile.php
@@ -3,3 +3,5 @@ dl("python.so");
 $python = new Python($argv);
 $python->assign("hello", ["a" => "b", "c" => "d"]);
 $python->evalFile(__DIR__.'/testArgv.py');
+
+$python->eval("print('EINE: Eval Is Not Evil')");
--- examples/testArgv.py
+++ examples/testArgv.py
@@ -1,3 +1,6 @@
 import sys
 print(sys.argv)
 print(hello)
+
+if __name__ == "__main__":
+    print("I am in main...")
